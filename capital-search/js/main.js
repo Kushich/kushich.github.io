68efc49d54b342e43336285bd7a6d2bff4d7d69f
Kushich <kushich@outlook.com>
2020-02-29 22:09:03 +0300

    add capital-search

---
+++ capital-search/js/main.js
@@ -0,0 +1,32 @@
+const search = document.getElementById('search')
+const matchList = document.getElementById('match-list')
+
+// Search countries.json and filter it
+const searchCountries = async searchText => {
+    const res = await fetch('../data/countries.json')
+    const countries = await res.json() 
+    
+    // Get matches te current text input
+    let matches = countries.filter(country => {
+        const regex = new RegExp(`^${searchText}`, 'gi')
+        return country.country.match(regex)
+    })
+    if (searchText.length === 0) {
+        matches = []
+        matchList.innerHTML = ''
+    }
+
+    outputHtml(matches)
+}
+const outputHtml = matches => {
+    if(matches.length > 0) {
+        const html = matches.map(match => `
+            <div class="card card-body mb-1">
+                <h4>${match.country} <span class="text-primary">${match.city}</span></h4>
+            </div>
+        `).join('')
+        matchList.innerHTML = html
+    }
+}
+
+search.addEventListener('input', () => searchCountries(search.value))
